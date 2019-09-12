5de1bda35471111de71d69bdb3fd3130867f00e9
Sebastian Andrew <sebastian.andrew@ovo.id>
2019-09-12 12:06:17 +0700

    first commit

---
+++ src/main.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    println("Hello World")
+    println("asd")
+
+    var tim: String
+    tim = "tim buakaka"
+    println(tim)
+
+    var timWeeklySalary: Int = 32
+    var timMonthlySalary: Int = timWeeklySalary * 4
+    println(timWeeklySalary)
+    println(timMonthlySalary)
+
+    println()
+    val apples: Int = 6
+    val oranges: Int = 5
+    var fruit: Int = apples - oranges
+    println(fruit)
+
+    println(apples / 4)
+
+    println()
+    val weeks: Int = 234
+    val years: Double = weeks / 52.0
+    println("$weeks weeks in years is: $years years")
+
+    println("My name is $tim")
+}
